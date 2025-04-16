d53c8d048b7589061ea786e219fb3d1797ca9e50
Harnek Bhalru <harnek_2000@hotmail.ca>
2025-04-16 19:26:46 -0400

    Updated API Logic

--- SuperHeroApp/api/superhero.js
+++ SuperHeroApp/api/superhero.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     const apiKey = process.env.EXPO_PUBLIC_API_KEY;
   
     try {
-      const response = await fetch(`/api/${apiKey}/search/${encodeURIComponent(q)}`);
+      const response = await fetch(`https://superheroapi.com/api/${apiKey}/search/${encodeURIComponent(q)}`);
   
       if (!response.ok) {
         throw new Error("Failed to fetch from Superhero API");
